05e2a2b2230646e8a51f282c0a7cd4f3fa76be43
VT Student <studentvt@hotmail.com>
2018-09-26 22:35:54 +0700

    Блокчейн создание функции кэша и ее проверки

--- BlockChain/BlockChain/BlockChain/Program.cs
+++ BlockChain/BlockChain/BlockChain/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using NBitcoin;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace BlockChain
 {
@@ -7,10 +8,26 @@ namespace BlockChain
     {
         static void Main(string[] args)
         {
+            string s = "Vasya";
+            Console.WriteLine(s);
+            string hashS = MD5Hash(s);
+            Console.WriteLine(hashS);
+            Console.ReadLine();
+
             
-            var key = new Key();
-            string s = 
-            Console.WriteLine(key.PubKey.GetAddress(Network.Main));
+
+        }
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
         }
     }
 }
