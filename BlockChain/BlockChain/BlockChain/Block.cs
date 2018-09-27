16e5f7be8e5fa633f7a15982cf624f5acc597c67
VT Student <studentvt@hotmail.com>
2018-09-27 23:33:42 +0700

    Добавлен класс блока и логика к нему

---
+++ BlockChain/BlockChain/BlockChain/Block.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlockChain
+{
+    public class Block
+    {
+        private string previosHash { get; set; }
+        private string  transaction { get; set; }
+        private List<string> contens { get; set; }
+
+        public Block(string previosHash, string transaction)
+        {
+            this.previosHash = previosHash;
+            this.transaction = transaction;
+
+            
+            contens.Add(MD5Hash(transaction));
+            contens.Add(previosHash);
+        }
+        private string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+
+    }
+    
+}
--- BlockChain/BlockChain/BlockChain/Program.cs
+++ BlockChain/BlockChain/BlockChain/Program.cs
@@ -10,24 +10,12 @@ namespace BlockChain
         {
             string s = "Vasya";
             Console.WriteLine(s);
-            string hashS = MD5Hash(s);
-            Console.WriteLine(hashS);
+            
             Console.ReadLine();
 
             
 
         }
-        public static string MD5Hash(string input)
-        {
-            StringBuilder hash = new StringBuilder();
-            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
-            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
-
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                hash.Append(bytes[i].ToString("x2"));
-            }
-            return hash.ToString();
-        }
+        
     }
 }
